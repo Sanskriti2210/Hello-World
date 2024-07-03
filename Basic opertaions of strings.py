ca72819a658e786b33af7b848b4152896907fc80
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-04 00:58:33 +0530

    Basic opertaions of strings.py

---
+++ Basic opertaions of strings.py
@@ -0,0 +1,22 @@
+a=int(input("Enter first no:"))
+b=int(input("Enter second no:"))
+
+print(a>=b)
+
+str1=("Isha")
+print(len(str1))
+str2=("Khush")
+final_str=str1+" "+str2
+print(len(final_str))
+
+print(final_str[1:6])
+
+Output:
+Enter first no:4
+Enter second no:6
+False
+4
+10
+sha K
+ 
+

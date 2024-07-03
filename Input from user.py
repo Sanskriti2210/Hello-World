f2b51f76634441a99c2528a53c8df4f627691c08
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-04 00:53:14 +0530

    Input from user.py

---
+++ Input from user.py
@@ -0,0 +1,15 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+marks=float(input("Enter your marks:"))
+
+print("Wlcome",name)
+print("My age is ",age)
+print("Marks= ",marks)
+
+Output:
+Enter your name:isha
+Enter your age:17
+Enter your marks:99
+Wlcome isha
+My age is  17
+Marks=  99.0

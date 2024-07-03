c4381703117a3c9f8ca90daee5be81e30d280e27
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-04 00:49:31 +0530

    Logical operator.py

---
+++ Logical operator.py
@@ -0,0 +1,17 @@
+c=50
+d=20
+print(not False)
+print(not (a>b))
+
+val1=True
+val2=False
+
+print("AND operator:", val1 and val2)
+
+print("OR operator:", val1 or val2)
+
+Output:
+True
+False
+AND operator: False
+OR operator: True

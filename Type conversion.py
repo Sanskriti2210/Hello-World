01196c217c61c65de51bf5e8ce847be330394012
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-04 00:51:40 +0530

    Type conversion.py

--- Type conversion.py
+++ Type conversion.py
@@ -13,7 +13,13 @@ a=str(a)
 print(type(a))
 print(sum)
 
+val=int(input("enter some value:"))
+print(type(val),val)
+
 Output:
 <class 'int'>
 <class 'str'>
 17.9
+
+enter some value:9
+<class 'int'> 9

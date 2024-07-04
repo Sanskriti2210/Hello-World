f3b045e1f1bb349a39873a725aefcb3b2262cf0b
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-05 00:08:34 +0530

    Find greatest of 3 no.py

---
+++ Find greatest of 3 no.py
@@ -0,0 +1,21 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+
+
+if(a>b and a>c):
+  print("A is greatest")
+
+elif(b>c):
+  print("B is greatest")
+
+else:
+  print("C is greatest")
+
+Output:
+Enter first number:4
+Enter second number:9
+Enter third number:7
+B is greatest
+ 
+

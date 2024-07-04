d3eb7aea56deea2d5f2a69868239da7189e54f52
Sanskriti2210 <160563663+Sanskriti2210@users.noreply.github.com>
2024-07-05 00:03:13 +0530

    Checking whether the number is even or odd.py

---
+++ Checking whether the number is even or odd.py
@@ -0,0 +1,16 @@
+num=int(input("enter the number:"))
+
+rem = num%2
+
+if(rem == 0):
+  print("The number is even")
+
+else:
+  print("The number is odd")
+
+Output:
+enter the number:56
+The number is even
+
+enter the number:89
+The number is odd
